96d7e9dab8b5b359f30fb0d996fe12251f0bbd58
anys34 <anyeseong34@gmail.com>
2024-06-08 15:27:29 +0900

    feat(#6): google feign
    - goole auth의 정보를 받아오기 위해 feign을 추가하였습니다.

---
+++ src/main/java/com/insert/ioj/global/feign/auth/res/GoogleInformationResponse.java
@@ -0,0 +1,11 @@
+package com.insert.ioj.global.feign.auth.res;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class GoogleInformationResponse {
+    private String email;
+    private String name;
+}
