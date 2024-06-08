43a149869ec9f2db372d51dea87893135c685c50
anys34 <anyeseong34@gmail.com>
2024-06-08 16:50:08 +0900

    feat(#6): accessToken 재발급
    - refreshToken을 제공하면 cache를 확인하여 일치한다면 accessToken 반환

---
+++ src/main/java/com/insert/ioj/domain/auth/presentation/dto/res/AccessTokenResponse.java
@@ -0,0 +1,8 @@
+package com.insert.ioj.domain.auth.presentation.dto.res;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class AccessTokenResponse {
+    private final String accessToken;
+}
