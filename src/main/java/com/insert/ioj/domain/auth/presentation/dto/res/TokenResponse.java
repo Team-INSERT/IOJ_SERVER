31ffae907058847d829936f560ab5cef5161c31c
anys34 <anyeseong34@gmail.com>
2024-06-11 20:11:12 +0900

    fix(#6): response dto에 @Getter 추가
    - response dto에서 직렬화를 하기 위해 @Getter를 추가하였습니다.

--- src/main/java/com/insert/ioj/domain/auth/presentation/dto/res/TokenResponse.java
+++ src/main/java/com/insert/ioj/domain/auth/presentation/dto/res/TokenResponse.java
@@ -1,7 +1,9 @@
 package com.insert.ioj.domain.auth.presentation.dto.res;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+@Getter
 @RequiredArgsConstructor
 public class TokenResponse {
     private final String accessToken;
