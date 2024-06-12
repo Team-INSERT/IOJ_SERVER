464ac91ebc1e502b25d80c0325e3e454fa3cea20
anys34 <anyeseong34@gmail.com>
2024-06-13 08:26:34 +0900

    feat(#6): 현재 인증된 유저 리턴
    - 현재 인증된 유저 user 리턴

--- src/main/java/com/insert/ioj/domain/user/facade/UserFacade.java
+++ src/main/java/com/insert/ioj/domain/user/facade/UserFacade.java
@@ -3,12 +3,18 @@ package com.insert.ioj.domain.user.facade;
 import com.insert.ioj.domain.user.domain.User;
 import com.insert.ioj.domain.user.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class UserFacade {
     private final UserRepository userRepository;
+
+    public User getCurrentUser() {
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        return getUserByEmail(email);
+    }
 
     public User getUserByEmail(String email) {
         return userRepository.findByEmail(email)
