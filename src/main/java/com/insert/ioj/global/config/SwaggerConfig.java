2c7ed46b0bcbe3ee1708dc41f53209f886657bc2
anys34 <anyeseong34@gmail.com>
2024-06-18 17:35:59 +0900

    feat(#11): Authorize 컴포넌트 추가
    - api에서 token 인증을 하기 위해 추가하였습니다.

--- src/main/java/com/insert/ioj/global/config/SwaggerConfig.java
+++ src/main/java/com/insert/ioj/global/config/SwaggerConfig.java
@@ -2,6 +2,11 @@ package com.insert.ioj.global.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
@@ -10,5 +15,17 @@ import org.springframework.context.annotation.Configuration;
 )
 @Configuration
 public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().addSecurityItem(new SecurityRequirement().
+                addList("Bearer Authentication"))
+            .components(new Components().addSecuritySchemes
+                ("Bearer Authentication", createAPIKeyScheme()));
+    }
 
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+            .bearerFormat("JWT")
+            .scheme("bearer");
+    }
 }
