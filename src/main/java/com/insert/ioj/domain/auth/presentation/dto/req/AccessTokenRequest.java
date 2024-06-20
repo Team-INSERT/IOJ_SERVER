3dadbfdbaccca1ad60cdf20672912c2589402fd2
J1W0N-1209 <nabook754@naver.com>
2024-06-20 19:58:07 +0900

    refactor(#15): Auth Dto 리팩토링
    Validation 추가

--- src/main/java/com/insert/ioj/domain/auth/presentation/dto/req/AccessTokenRequest.java
+++ src/main/java/com/insert/ioj/domain/auth/presentation/dto/req/AccessTokenRequest.java
@@ -1,10 +1,12 @@
 package com.insert.ioj.domain.auth.presentation.dto.req;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class AccessTokenRequest {
+    @NotNull(message = "accessToken은 null일 수 없습니다.")
     private String accessToken;
 }
--- src/main/java/com/insert/ioj/domain/auth/presentation/dto/req/CreateAccessTokenRequest.java
+++ src/main/java/com/insert/ioj/domain/auth/presentation/dto/req/CreateAccessTokenRequest.java
@@ -1,10 +1,12 @@
 package com.insert.ioj.domain.auth.presentation.dto.req;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class CreateAccessTokenRequest {
+    @NotNull(message = "refreshToken은 null일 수 없습니다.")
     private String refreshToken;
 }
