95dbf8686e59a4199bee4591bd37011cdf1459a5
anys34 <anyeseong34@gmail.com>
2024-06-21 14:54:11 +0900

    feat(#7): not found user 에러 처리
    - 사용자가 존재하지 않을 때의 에러를 처리하였습니다.

--- src/main/java/com/insert/ioj/global/error/exception/ErrorCode.java
+++ src/main/java/com/insert/ioj/global/error/exception/ErrorCode.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
+    NOT_FOUND_USER(404, "USER-404-1", "요청한 사용자가 존재하지 않습니다."),
 
     INTERNAL_SERVER_ERROR(500, "SERVER-500-1", "서버 에러가 발생하였습니다. 관리자에게 문의해 주세요.");
 
