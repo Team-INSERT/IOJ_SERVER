7233e1236277b6b6beb4ac8596ba92c742a9c766
anys34 <anyeseong34@gmail.com>
2024-06-23 17:41:53 +0900

    feat(#7): validation 추가
    - validation을 사용하기 위해 종속하였습니다
    - dto에 validation을 사용하여 유효성 검사를 하였습니다.

--- src/main/java/com/insert/ioj/domain/auth/presentation/dto/req/CreateAccessTokenRequest.java
+++ src/main/java/com/insert/ioj/domain/auth/presentation/dto/req/CreateAccessTokenRequest.java
@@ -1,10 +1,12 @@
 package com.insert.ioj.domain.auth.presentation.dto.req;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class CreateAccessTokenRequest {
+    @NotNull(message = "refreshToken이 비어있습니다.")
     private String refreshToken;
 }
