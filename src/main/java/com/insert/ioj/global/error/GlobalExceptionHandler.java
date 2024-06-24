666de2b828dfbfbd5c1a3a9f7b947dd0a2f66555
anys34 <anyeseong34@gmail.com>
2024-06-24 19:34:53 +0900

    feat(#7): GlobalValidationException추가
    - validation에서 에러난 것을 전역에서 처리하여 반환

--- src/main/java/com/insert/ioj/global/error/GlobalExceptionHandler.java
+++ src/main/java/com/insert/ioj/global/error/GlobalExceptionHandler.java
@@ -5,8 +5,14 @@ import com.insert.ioj.global.error.exception.IojException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestControllerAdvice
@@ -24,5 +30,20 @@ public class GlobalExceptionHandler {
             response,
             HttpStatus.valueOf(errorCode.getStatus())
         );
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Map<String, String>>> handleBadRequestExceptions(MethodArgumentNotValidException e) {
+        Map<String, String> errors = e.getBindingResult().getFieldErrors()
+            .stream()
+            .collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage));
+
+        return new ResponseEntity<>(getErrorsMap(errors), HttpStatus.BAD_REQUEST);
+    }
+
+    private Map<String, Map<String, String>> getErrorsMap(Map<String, String> errors) {
+        Map<String, Map<String, String>> errorResponse = new HashMap<>();
+        errorResponse.put("errors", errors);
+        return errorResponse;
     }
 }
