013084f8ee43f8d326302c4027aa31429a4c606a
anys34 <anyeseong34@gmail.com>
2024-06-25 14:35:33 +0900

    refactor(#7): 반환 에러 코드 수정
    - 403을 리턴하던 에러코드를 적절한 401로 변경

--- src/main/java/com/insert/ioj/global/error/exception/ErrorCode.java
+++ src/main/java/com/insert/ioj/global/error/exception/ErrorCode.java
@@ -8,8 +8,8 @@ import lombok.Getter;
 public enum ErrorCode {
     NOT_FOUND_USER(404, "USER-404-1", "요청한 사용자가 존재하지 않습니다."),
 
-    INVALID_TOKEN(403, "TOKEN-403-1", "올바르지 않은 형식의 Token입니다."),
-    EXPIRED_PERIOD_TOKEN(403, "TOKEN-403-2", "기한이 만료된 Token입니다."),
+    INVALID_TOKEN(401, "TOKEN-401-1", "올바르지 않은 형식의 Token입니다."),
+    EXPIRED_PERIOD_TOKEN(401, "TOKEN-401-2", "기한이 만료된 Token입니다."),
 
     INTERNAL_SERVER_ERROR(500, "SERVER-500-1", "서버 에러가 발생하였습니다. 관리자에게 문의해 주세요.");
 
