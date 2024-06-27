692ab2a2be92a400334efd4e609cf81baf71cf25
anys34 <anyeseong34@gmail.com>
2024-06-28 08:28:06 +0900

    refactor(#18): class response로 변경
    - record형식에서 class형식의 response로 변경

--- src/main/java/com/insert/ioj/domain/user/presentation/dto/res/InfoUserResponse.java
+++ src/main/java/com/insert/ioj/domain/user/presentation/dto/res/InfoUserResponse.java
@@ -2,15 +2,17 @@ package com.insert.ioj.domain.user.presentation.dto.res;
 
 import com.insert.ioj.domain.user.domain.User;
 import com.insert.ioj.domain.user.domain.type.Color;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
-public record InfoUserResponse(
-    String nickname,
-    Color color
-) {
+@Getter
+@RequiredArgsConstructor
+public class InfoUserResponse {
+    private String nickname;
+    private Color color;
+
     public InfoUserResponse(User user) {
-        this(
-            user.getNickname(),
-            user.getColor()
-        );
+        this.nickname = user.getNickname();
+        this.color = user.getColor();
     }
 }
