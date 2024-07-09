e513075244810f1d387bc91bdea1d98422d6e44a
anys34 <anyeseong34@gmail.com>
2024-07-10 00:19:16 +0900

    feat(#25): swagger 대회 도메인 추가
    - 대회 도메인 api를 swagger에 추가햐였습니다.

--- src/main/java/com/insert/ioj/domain/competition/presentation/CompetitionController.java
+++ src/main/java/com/insert/ioj/domain/competition/presentation/CompetitionController.java
@@ -2,6 +2,8 @@ package com.insert.ioj.domain.competition.presentation;
 
 import com.insert.ioj.domain.competition.presentation.dto.req.SaveCompetitionRequest;
 import com.insert.ioj.domain.competition.service.SaveCompetitionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,12 +11,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Competition API")
 @RequiredArgsConstructor
 @RequestMapping("/competition")
 @RestController
 public class CompetitionController {
     private final SaveCompetitionService saveCompetitionService;
 
+    @Operation(summary = "대회 생성")
     @PostMapping
     public Long saveCompetition(@RequestBody @Valid SaveCompetitionRequest request) {
         return saveCompetitionService.execute(request);
