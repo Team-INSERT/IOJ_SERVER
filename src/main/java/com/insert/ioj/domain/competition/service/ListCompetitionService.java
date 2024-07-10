3e1fedc971b186524c042e8e27fa1b0bf28c9713
anys34 <anyeseong34@gmail.com>
2024-07-11 01:02:02 +0900

    refactor(#25): transactional 추가
    - ListCompetitionService부분에 읽기 전용 transactional을 붙이지 않아 추가하였습니다.

--- src/main/java/com/insert/ioj/domain/competition/service/ListCompetitionService.java
+++ src/main/java/com/insert/ioj/domain/competition/service/ListCompetitionService.java
@@ -5,6 +5,7 @@ import com.insert.ioj.domain.competition.presentation.dto.res.ListCompetitionRes
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -14,6 +15,7 @@ import java.util.stream.Collectors;
 public class ListCompetitionService {
     private final CompetitionRepository competitionRepository;
 
+    @Transactional(readOnly = true)
     public List<ListCompetitionResponse> execute(Pageable pageable) {
         return competitionRepository.findAll(pageable).stream()
             .map(ListCompetitionResponse::new)
