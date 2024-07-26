236385f20a8563bf72f98e226ecdbb8784e2f93b
anys34 <anyeseong34@gmail.com>
2024-07-26 16:59:57 +0900

    feat(#48): copyfile 메서드 추가
    - 파일을 복사할 수 있는 메서드를 추가하였습니다.

--- src/main/java/com/insert/ioj/infra/file/FileUtil.java
+++ src/main/java/com/insert/ioj/infra/file/FileUtil.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 public class FileUtil {
     public static void createDirectory(String path) throws IOException {
@@ -15,6 +17,12 @@ public class FileUtil {
         try(OutputStream os = new FileOutputStream(path)) {
             os.write(content.getBytes());
         }
+    }
+
+    public static void copyFile(String src, String dest) throws IOException {
+        Path copied = Paths.get(dest);
+        Path originalPath = Paths.get(src);
+        Files.copy(originalPath, copied, StandardCopyOption.REPLACE_EXISTING);
     }
 
     public static void deleteFile(String path) throws IOException {
