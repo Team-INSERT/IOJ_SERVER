01958d27fc0d185c22284a5868c3f6540b45b83a
anys34 <anyeseong34@gmail.com>
2024-07-26 17:39:47 +0900

    feat(#48): file 상수 수정
    - executions/*에서 executions/utility_*로 변경하였습니다.

--- src/main/java/com/insert/ioj/global/constants/FolderConstants.java
+++ src/main/java/com/insert/ioj/global/constants/FolderConstants.java
@@ -4,8 +4,8 @@ import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class FolderConstants {
-    public static final String C_EXECUTION_FOLDER_NAME = "/executions/c";
-    public static final String CPP_EXECUTION_FOLDER_NAME = "/executions/cpp";
-    public static final String JAVA_EXECUTION_FOLDER_NAME = "/executions/java";
-    public static final String PYTHON_EXECUTION_FOLDER_NAME = "/executions/python";
+    public static final String C_EXECUTION_FOLDER_NAME = "executions/utility_c";
+    public static final String CPP_EXECUTION_FOLDER_NAME = "executions/utility_cpp";
+    public static final String JAVA_EXECUTION_FOLDER_NAME = "executions/utility_java";
+    public static final String PYTHON_EXECUTION_FOLDER_NAME = "executions/utility_py";
 }
