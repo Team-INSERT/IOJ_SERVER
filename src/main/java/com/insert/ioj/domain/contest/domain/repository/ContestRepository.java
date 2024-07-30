87deecc5d13ee7cbf1e5dac1b1085fc4b3babf3c
anys34 <anyeseong34@gmail.com>
2024-07-30 21:35:08 +0900

    feat(#54): 어드민용 대회 리스트 추가
    - 어드민 페이지에서 사용할 대회 리스트 api를 만들었습니다.

--- src/main/java/com/insert/ioj/domain/contest/domain/repository/ContestRepository.java
+++ src/main/java/com/insert/ioj/domain/contest/domain/repository/ContestRepository.java
@@ -1,7 +1,14 @@
 package com.insert.ioj.domain.contest.domain.repository;
 
 import com.insert.ioj.domain.contest.domain.Contest;
+import com.insert.ioj.domain.contest.presentation.dto.res.ListContestAdminResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface ContestRepository extends JpaRepository<Contest, Long> {
+    List<Contest> findAllByEndTimeAfter(LocalDateTime now);
 }
