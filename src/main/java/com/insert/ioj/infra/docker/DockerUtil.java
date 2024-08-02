245766af30732a39e59f463b99e9a4016926f307
anys34 <anyeseong34@gmail.com>
2024-08-03 02:52:06 +0900

    refactor(#48): docker process time 수정
    - build time은 30초로 수정
    - run time은 20초로 수정

--- src/main/java/com/insert/ioj/infra/docker/DockerUtil.java
+++ src/main/java/com/insert/ioj/infra/docker/DockerUtil.java
@@ -4,8 +4,8 @@ import com.insert.ioj.infra.cmd.CmdUtil;
 import com.insert.ioj.infra.cmd.dto.res.ProcessOutput;
 
 public class DockerUtil {
-    public static final int BUILD_TIMEOUT = 60;
-    public static final int RUN_TIMEOUT = 60;
+    public static final int BUILD_TIMEOUT = 30;
+    public static final int RUN_TIMEOUT = 20;
 
     public static void buildImage(String dockerfilePath, String id, String contextPath) {
         String[] dockerCommand =
