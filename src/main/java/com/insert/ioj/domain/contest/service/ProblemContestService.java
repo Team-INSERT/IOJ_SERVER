365f79d34948beca7f187362f7b922c2b63a9d5f
anys34 <anyeseong34@gmail.com>
2024-08-04 02:47:20 +0900

    refactor(#47): getContestProblemsWithStatus 수정
    - JPQL로 만들었던 메서드를 querydsl로 변경하였습니다.

--- src/main/java/com/insert/ioj/domain/contest/service/ProblemContestService.java
+++ src/main/java/com/insert/ioj/domain/contest/service/ProblemContestService.java
@@ -3,7 +3,7 @@ package com.insert.ioj.domain.contest.service;
 import com.insert.ioj.domain.contest.domain.Contest;
 import com.insert.ioj.domain.contest.facade.ContestFacade;
 import com.insert.ioj.domain.contest.presentation.dto.res.ListContestProblemResponse;
-import com.insert.ioj.domain.problemContest.domain.repository.ProblemContestRepository;
+import com.insert.ioj.domain.problemContest.domain.repository.CustomCompetitionContestRepository;
 import com.insert.ioj.domain.user.domain.User;
 import com.insert.ioj.domain.user.facade.UserFacade;
 import lombok.RequiredArgsConstructor;
@@ -16,12 +16,12 @@ import java.util.List;
 public class ProblemContestService {
     private final UserFacade userFacade;
     private final ContestFacade contestFacade;
-    private final ProblemContestRepository problemContestRepository;
+    private final CustomCompetitionContestRepository customProblemContestRepository;
 
     public List<ListContestProblemResponse> execute(Long contestId) {
         User user = userFacade.getCurrentUser();
         Contest contest = contestFacade.getContest(contestId);
         contest.checkRole(user.getAuthority());
-        return problemContestRepository.getContestProblemsWithStatus(contest, user);
+        return customProblemContestRepository.getContestProblemsWithStatus(contest, user);
     }
 }
