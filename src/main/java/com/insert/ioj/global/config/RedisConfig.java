a25b210712b6834d9d2c156a231624f32095f3d7
anys34 <anyeseong34@gmail.com>
2024-09-03 19:11:45 +0900

    feat(#117): redis Config
    - redis를 연결하기 위해 redisConfig를 작성하였습니다.

---
+++ src/main/java/com/insert/ioj/global/config/RedisConfig.java
@@ -0,0 +1,25 @@
+package com.insert.ioj.global.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+@Configuration
+public class RedisConfig {
+    @Value("${spring.data.redis.host}") private String redisHost;
+
+    @Value("${spring.data.redis.port}") private int redisPort;
+
+    @Value("${spring.data.redis.password}") private String redisPassword;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(redisHost, redisPort);
+        if (redisPassword != null && !redisPassword.isBlank()) config.setPassword(redisPassword);
+
+        return new LettuceConnectionFactory(config);
+    }
+}
