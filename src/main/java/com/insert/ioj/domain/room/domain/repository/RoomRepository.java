72da4b3f9a81fea07663f960267493734573adaa
anys34 <anyeseong34@gmail.com>
2024-09-11 16:46:57 +0900

    refactor(#125): room repository Id를 UUID로 변경
    - room entity Id가 Long에서 UUID로 변경이 되어 UUID로 수정하였습니다.

--- src/main/java/com/insert/ioj/domain/room/domain/repository/RoomRepository.java
+++ src/main/java/com/insert/ioj/domain/room/domain/repository/RoomRepository.java
@@ -3,5 +3,7 @@ package com.insert.ioj.domain.room.domain.repository;
 import com.insert.ioj.domain.room.domain.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RoomRepository extends JpaRepository<Room, Long> {
+import java.util.UUID;
+
+public interface RoomRepository extends JpaRepository<Room, UUID> {
 }
