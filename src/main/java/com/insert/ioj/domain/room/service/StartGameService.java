20782c4ebffce7e294d1a15b043d8f3d081feef4
anys34 <anyeseong34@gmail.com>
2024-09-13 23:54:33 +0900

    feat(#125): 준비하지 않은 유저가 있을 시 예외
    - 준비 하지 않은 유저가 있을 시 게임을 시작하지 못 하게 하였습니다.

--- src/main/java/com/insert/ioj/domain/room/service/StartGameService.java
+++ src/main/java/com/insert/ioj/domain/room/service/StartGameService.java
@@ -1,14 +1,19 @@
 package com.insert.ioj.domain.room.service;
 
+import com.insert.ioj.domain.entry.domain.Entry;
+import com.insert.ioj.domain.entry.domain.repository.EntryRepository;
 import com.insert.ioj.domain.room.domain.Room;
 import com.insert.ioj.domain.room.facade.RoomFacade;
 import com.insert.ioj.domain.room.presentation.dto.res.StartGameResponse;
 import com.insert.ioj.domain.user.domain.User;
 import com.insert.ioj.domain.user.facade.UserFacade;
+import com.insert.ioj.global.error.exception.ErrorCode;
+import com.insert.ioj.global.error.exception.IojException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @RequiredArgsConstructor
@@ -16,6 +21,7 @@ import java.util.UUID;
 public class StartGameService {
     private final RoomFacade roomFacade;
     private final UserFacade userFacade;
+    private final EntryRepository entryRepository;
 
     @Transactional
     public StartGameResponse execute(UUID roomId) {
@@ -23,8 +29,17 @@ public class StartGameService {
         User user = userFacade.getCurrentUser();
 
         room.checkHost(user);
+        checkReadyUser(room);
         room.updateStatus();
 
         return new StartGameResponse();
+    }
+
+    private void checkReadyUser(Room room) {
+        Long cntIsReady = entryRepository.countIsReady(room)
+            .orElseThrow(() -> new IojException(ErrorCode.NOT_FOUND_ROOM_IN_USER));
+        if (room.getMaxPeople()-1 != cntIsReady) {
+            throw new IojException(ErrorCode.NOT_READY_ROOM);
+        }
     }
 }
--- src/main/java/com/insert/ioj/global/error/exception/ErrorCode.java
+++ src/main/java/com/insert/ioj/global/error/exception/ErrorCode.java
@@ -8,6 +8,7 @@ import lombok.Getter;
 public enum ErrorCode {
     ALREADY_USER(400, "ROOM-400-1", "이미 유저가 방에 참가중입니다."),
     FULL_ROOM(400, "ROOM-400-2", "방에 유저가 가득 찼습니다."),
+    NOT_READY_ROOM(400, "ROOM-400-3", "방에 준비를 하지 않은 유저가 있습니다."),
 
     PROCESS_EXECUTION(500, "EXECUTION-1", "명령어를 실행하는 도중 에러가 발생하였습니다."),
 
@@ -22,6 +23,7 @@ public enum ErrorCode {
     NOT_FOUND_CONTEST(404, "CONTEST-404-1", "요청한 대회가 존재하지 않습니다."),
     NOT_FOUND_LANGUAGE(404, "LANGUAGE-404-1", "요청한 언어가 존재하지 않습니다."),
     NOT_FOUND_ROOM(404, "ROOM-404-1", "요청한 방이 존재하지 않습니다."),
+    NOT_FOUND_ROOM_IN_USER(404, "ROOM-404-2", "방 안에 유저가 존재하지 않습니다."),
 
     INVALID_TOKEN(401, "TOKEN-401-1", "올바르지 않은 형식의 Token입니다."),
     EXPIRED_PERIOD_TOKEN(401, "TOKEN-401-2", "기한이 만료된 Token입니다."),
