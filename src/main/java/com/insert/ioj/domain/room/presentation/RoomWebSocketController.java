bccd854de3bae828b7dc4b294b5a94e56026b229
anys34 <anyeseong34@gmail.com>
2024-09-13 22:42:55 +0900

    refactor(#125): publish 방식 수정
    - @sendTo가 작동을 하지 않아 messgingTemplate방식으로 publish합니다.

--- src/main/java/com/insert/ioj/domain/room/presentation/RoomWebSocketController.java
+++ src/main/java/com/insert/ioj/domain/room/presentation/RoomWebSocketController.java
@@ -1,20 +1,20 @@
 package com.insert.ioj.domain.room.presentation;
 
-import com.insert.ioj.domain.room.presentation.dto.res.*;
 import com.insert.ioj.domain.room.service.*;
 import lombok.RequiredArgsConstructor;
-import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.stereotype.Controller;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
 @RequiredArgsConstructor
 @RequestMapping("/room")
-@Controller
+@RestController
 public class RoomWebSocketController {
+    private final SimpMessagingTemplate messagingTemplate;
     private final JoinRoomService joinRoomService;
     private final LeaveRoomService leaveRoomService;
     private final ReadyService readyService;
@@ -22,32 +22,27 @@ public class RoomWebSocketController {
     private final DeleteRoomService deleteRoomService;
 
     @GetMapping("/{roomId}/join")
-    @SendTo("/topic/room/{roomId}")
-    public JoinRoomResponse joinRoom(@PathVariable UUID roomId) {
-        return joinRoomService.execute(roomId);
+    public void joinRoom(@PathVariable UUID roomId) {
+        messagingTemplate.convertAndSend("/topic/room/" + roomId, joinRoomService.execute(roomId));
     }
 
     @GetMapping("/{roomId}/leave")
-    @SendTo("/topic/room/{roomId}")
-    public LeaveRoomResponse leaveRoom(@PathVariable UUID roomId) {
-        return leaveRoomService.execute(roomId);
+    public void leaveRoom(@PathVariable UUID roomId) {
+        messagingTemplate.convertAndSend("/topic/room/" + roomId, leaveRoomService.execute(roomId));
     }
 
     @GetMapping("/{roomId}/ready")
-    @SendTo("/topic/room/{roomId}")
-    public ReadyResponse ready(@PathVariable UUID roomId) {
-        return readyService.execute(roomId);
+    public void ready(@PathVariable UUID roomId) {
+        messagingTemplate.convertAndSend("/topic/room/" + roomId, readyService.execute(roomId));
     }
 
     @GetMapping("/{roomId}/start")
-    @SendTo("/topic/room/{roomId}")
-    public StartGameResponse startGame(@PathVariable UUID roomId) {
-        return startGameService.execute(roomId);
+    public void startGame(@PathVariable UUID roomId) {
+        messagingTemplate.convertAndSend("/topic/room/" + roomId, startGameService.execute(roomId));
     }
 
     @GetMapping("/{roomId}/delete")
-    @SendTo("/topic/room/{roomId}")
-    public DeleteRoomResponse deleteRoom(@PathVariable UUID roomId) {
-        return deleteRoomService.execute(roomId);
+    public void deleteRoom(@PathVariable UUID roomId) {
+        messagingTemplate.convertAndSend("/topic/room/" + roomId, deleteRoomService.execute(roomId));
     }
 }
