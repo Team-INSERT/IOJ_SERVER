af4b14a8f23c8bfdc4a2f283313f077d3b4e01b8
anys34 <anyeseong34@gmail.com>
2024-10-04 18:31:47 +0900

    feat(#179): baseTimeEntity 수정시간 속성 추가
    - baseTimeEntity에 수정시간 속성을 추가하였습니다
    - abstract class로 변경하였습니다

--- src/main/java/com/insert/ioj/global/entity/BaseTimeEntity.java
+++ src/main/java/com/insert/ioj/global/entity/BaseTimeEntity.java
@@ -1,9 +1,11 @@
 package com.insert.ioj.global.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -11,7 +13,12 @@ import java.time.LocalDateTime;
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-public class BaseTimeEntity {
+public abstract class BaseTimeEntity {
     @CreatedDate
-    private LocalDateTime createDate;
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 }
