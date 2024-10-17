54e33750ba522244e72a14848a3af25abb979a72
anys34 <anyeseong34@gmail.com>
2024-10-18 08:12:50 +0900

    feat(#124): solve Repository 추가
    - room에서 문제를 제출하였을 때 정보를 저장하는 entity를 만들었습니다.

---
+++ src/main/java/com/insert/ioj/domain/solve/room/SolveRoom.java
@@ -0,0 +1,34 @@
+package com.insert.ioj.domain.solve.room;
+
+
+import com.insert.ioj.domain.execution.domain.type.Verdict;
+import com.insert.ioj.domain.execution.language.Language;
+import com.insert.ioj.domain.problem.domain.Problem;
+import com.insert.ioj.domain.room.domain.Room;
+import com.insert.ioj.domain.solve.solve.Solve;
+import com.insert.ioj.domain.user.domain.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class SolveRoom extends Solve {
+    @ManyToOne
+    @JoinColumn(name = "room_id")
+    private Room room;
+
+    public SolveRoom(User user,
+                     Room room,
+                     Problem problem,
+                     String sourcecode,
+                     Verdict verdict,
+                     Language language) {
+        super(user, problem, sourcecode, verdict, language);
+        this.room = room;
+    }
+}
