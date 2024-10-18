d611c87255c9bf95ccc8d7c67ad22eb7d29ef680
anys34 <anyeseong34@gmail.com>
2024-10-18 10:30:24 +0900

    feat(#124): item entity 추가
    - item entity에서 지급받은 아이템, 사용여부 등 다양한 정보를 저장하는 entity를 만들었습니다.

---
+++ src/main/java/com/insert/ioj/domain/item/domain/UserItem.java
@@ -0,0 +1,50 @@
+package com.insert.ioj.domain.item.domain;
+
+import com.insert.ioj.domain.item.domain.type.Item;
+import com.insert.ioj.domain.room.domain.Room;
+import com.insert.ioj.domain.user.domain.User;
+import com.insert.ioj.global.entity.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class UserItem extends BaseTimeEntity {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private Item item;
+
+    private Boolean used = false;
+
+    private Boolean booked = false;
+
+    @Column(name = "used_at")
+    private LocalDateTime usedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "room_id")
+    private Room room;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "target_user_id")
+    private User targetUser;
+}
