eb2e5b30d3cf70b7e9fe7b189fdb270c9d5e873c
anys34 <anyeseong34@gmail.com>
2024-11-18 10:36:33 +0900

    feat(#232): 유효성 검증 추가
    - 공격 시작 api 사용을 공격 받은 사람만 사용할 수 있도록 유효성 검증을 추가하였습니다.

--- src/main/java/com/insert/ioj/domain/item/service/AttackStartService.java
+++ src/main/java/com/insert/ioj/domain/item/service/AttackStartService.java
@@ -2,6 +2,8 @@ package com.insert.ioj.domain.item.service;
 
 import com.insert.ioj.domain.item.domain.UserItem;
 import com.insert.ioj.domain.item.domain.repository.UserItemRepository;
+import com.insert.ioj.domain.user.domain.User;
+import com.insert.ioj.domain.user.facade.UserFacade;
 import com.insert.ioj.global.error.exception.ErrorCode;
 import com.insert.ioj.global.error.exception.IojException;
 import lombok.RequiredArgsConstructor;
@@ -11,12 +13,18 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Service
 public class AttackStartService {
+    private final UserFacade userFacade;
     private final UserItemRepository userItemRepository;
 
     @Transactional
     public void execute(Long attackItemId) {
+        User user = userFacade.getCurrentUser();
         UserItem userItem = userItemRepository.findById(attackItemId)
             .orElseThrow(() -> new IojException(ErrorCode.NOT_FOUND_ITEM));
+
+        if (!userItem.getTargetUser().equals(user)) {
+            throw new IojException(ErrorCode.NOT_MATCH_USER_ITEM);
+        }
 
         userItem.attackStart();
     }
