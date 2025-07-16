cc155c9ed8e9f9d3a991e6126ffbd8936e013e21
anys34 <anyeseong34@gmail.com>
2025-07-17 08:39:19 +0900

    feat: contest

--- src/main/java/com/insert/ioj/domain/contest/presentation/dto/res/ListContestResponse.java
+++ src/main/java/com/insert/ioj/domain/contest/presentation/dto/res/ListContestResponse.java
@@ -3,11 +3,13 @@ package com.insert.ioj.domain.contest.presentation.dto.res;
 import com.insert.ioj.domain.contest.domain.Contest;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor
+@ToString
 public class ListContestResponse {
     private Long id;
     private String title;
--- src/main/java/com/insert/ioj/domain/contest/service/ListContestService.java
+++ src/main/java/com/insert/ioj/domain/contest/service/ListContestService.java
@@ -7,7 +7,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -18,7 +17,7 @@ public class ListContestService {
 
     @Transactional(readOnly = true)
     public List<ListContestResponse> execute(Pageable pageable) {
-        return contestRepository.findAllByEndTimeAfter(LocalDateTime.now(), pageable).stream()
+        return contestRepository.findAll(pageable).stream()
             .map(ListContestResponse::new)
             .collect(Collectors.toList());
     }
